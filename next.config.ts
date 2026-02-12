e999a3cd68412cefdf5890f0d274ce76ea92df8c
Serhii Surzhykov <sergeysurzhikov2@gmail.com>
2026-02-12 11:19:47 +0300

    fix(basePath for GitHub Pages)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/my-website',
+  basePath: '/personal-website',
   images: {
     unoptimized: true
   }
